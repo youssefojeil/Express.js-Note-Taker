003f9a793ae08054e3b111e868258f47116043aa
unknown <youssef.ojeil@hotmail.com>
2022-10-30 21:28:16 -0400

    change port to 3000, add middleware for static file & json parsing

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const path = require("path");
 
+// invoke express & set port to 3000
 const app = express();
+const PORT = 3000;
 
-app.listen(3001, () => {
-    console.log("Listening on port 3001");
+// Adding middleware for json parsing & static file
+app.use(express.json());
+app.use(express.static("public"));
+
+// get homepage (index.html)
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// listen on port 3000
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
 })
